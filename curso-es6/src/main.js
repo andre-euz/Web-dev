29a142d3490b2a5c45063473a6962cc929e7fbe4
André Euzébio <andre.r.euzebio@gmail.com>
2020-01-28 14:54:40 -0300

    Segunda parte exercicio conversão do código para async/awwait

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-// Funão delay aciona o .then após 1s
-//const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
-var i = 1;
-const delay = () => new Promise((resolve,reject) => {
-    setTimeout(() => { resolve(i+++'s')},2000);
-});
+async function getUserFromGithub(user) {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${user}`);
+        return console.log(response.data);
+    } catch(err) {
+        console.warn(`Usuário não encontrado! ${err}`);
+    }
+   
     
-async function umPorSegundo() {
-    console.log(await delay());      
-    console.log(await delay());      
-    console.log(await delay());      
 }
-umPorSegundo();
- 
+
+getUserFromGithub('andre-euz');
+getUserFromGithub('andre-euz23232');
+  
