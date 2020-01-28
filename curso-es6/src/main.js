bdbe611be28f1348cdc095856dcab074934131a9
André Euzébio <andre.r.euzebio@gmail.com>
2020-01-28 08:20:48 -0300

    Exercicio utilização de async/await

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,5 +1,16 @@
-import ClasseUsuario, {idade as IdadeUsuario} from '../functions';
+import axios from 'axios';
 
-ClasseUsuario.info();
-
-console.log(`A idade é ${IdadeUsuario} anos`);
+// Funão delay aciona o .then após 1s
+//const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+var i = 1;
+const delay = () => new Promise((resolve,reject) => {
+    setTimeout(() => { resolve(i+++'s')},2000);
+});
+    
+async function umPorSegundo() {
+    console.log(await delay());      
+    console.log(await delay());      
+    console.log(await delay());      
+}
+umPorSegundo();
+ 
