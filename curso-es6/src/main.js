e65c7c32243325e87af00edd76f4dafca14aa1d1
andre-euz <andre.r.euzebio@gmail.com>
2020-02-15 20:19:41 -0300

    Application to return information of repositories in github, using axios to receive via JSON and presenting to user

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,17 +1,90 @@
-import axios from 'axios';
+import api from './api';
 
-const buscaUsuario = async (usuario) => {
-    try {
-        const response = await axios.get(`https://api.github.com/users/${usuario}`);
-        console.log(response.data);
-    } catch (err) {
-        console.warn(`Usuário não existe! ${err}`);
+class App {
+    constructor() {
+        this.repositories = []; 
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() { 
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    setLoading(loading = true) {
+        if(loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id','loading');
+
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById('loading').remove();
+        }
+    }
+    async addRepository(event) {
+        event.preventDefault();  
+
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0)
+            return;
+
+        this.setLoading();    
+        try {        
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const {name, description, html_url, owner: { avatar_url }} = response.data;
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+
+            this.inputEl.value = '';
+            this.render();    
+        } catch (err) {
+            alert('O repositório informado não existe!');
+        }
+        this.setLoading(false); 
+    } 
+    render() {
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('href',repo.html_url);
+            linkEl.setAttribute('target','_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEL = document.createElement('li');
+            listItemEL.appendChild(imgEl);
+            listItemEL.appendChild(titleEl);
+            listItemEL.appendChild(descriptionEl);
+            listItemEL.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEL);
+ 
+        })
     }
 }
 
-buscaUsuario('andre-euz');
-buscaUsuario('andre-euz2222');
 
+new App();
 
 
   
